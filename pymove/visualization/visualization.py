c771c049a2a843983ce447de2b577b50b173b270
andyfernandes <andrezafernandes@alu.ufc.br>
2019-09-05 23:30:23 -0300

    [+] Documentação inicio visualization.py

--- pymove/visualization/visualization.py
+++ pymove/visualization/visualization.py
@@ -1,56 +1,84 @@
 # TODO: Andreza
-import matplotlib
-import matplotlib.pyplot as plt
+import folium
 import colorsys
 import numpy as np
-import folium
+# import matplotlib
+import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
+from pymove.utils.utils import dic_features_label, dic_labels
+from pymove.utils.transformations import create_update_date_features, create_update_hour_features, create_update_day_of_the_week_features, create_update_time_of_day_features
 
-from pymove import trajutils
+def rgb(rgb_colors):
+    """
+    Return a tuple of integers, as used in AWT/Java plots.
 
-# http://www.color-hex.com/color/
+    Parameters
+    ----------
+    rgb_colors : list
+        Represents a list with three positions that correspond to the percentage red, green and blue colors.
 
-def rgb(RGBcolors):
-    """ Return a tuple of integers, as used in AWT/Java plots. """
-    blue  = RGBcolors[0]
-    red   = RGBcolors[1]
-    green = RGBcolors[2]
-    return int(red*255), int(green*255), int(blue*255)
+    Returns
+    -------
+    tuple_rgb : tuple
+        Represents a tuple of integers that correspond the colors values.
 
-def hexRgb(RGBcolors):
-    """ Return a hex string, as used in Tk plots. """
-    return "#%02X%02X%02X" % rgb(RGBcolors)
+    Examples
+    --------
+    >>> from pymove.visualization.visualization import rgb
+    >>> rgb([0.6,0.2,0.2])
+        (51, 51, 153)
+
+    """
+    blue  = rgb_colors[0]
+    red   = rgb_colors[1]
+    green = rgb_colors[2]
+    tuple_rgb = int(red*255), int(green*255), int(blue*255)
+    return tuple_rgb
+
+def hex_rgb(rgb_colors):
+    """
+    Return a hex string, as used in Tk plots.
+
+    Parameters
+    ----------
+    rgb_colors : list
+        Represents a list with three positions that correspond to the percentage red, green and blue colors.
+
+    Returns
+    -------
+    hex_colors : String
+        Represents a color in hexadecimal format.
+
+    Examples
+    --------
+    >>> from pymove.visualization.visualization import hex_rgb
+    >>> hex_rgb([0.6,0.2,0.2])
+    '#333399'
+
+    """
+    hex_colors = "#%02X%02X%02X" % rgb(rgb_colors)
+    return hex_colors
 
 def cmap_hex_color(cmap, i):
     return matplotlib.colors.rgb2hex(cmap(i))
 
-def save_map(df, file, tiles='OpenStreetMap', label_id=trajutils.dic_labels['id'], dic_labels = trajutils.dic_labels, cmap='tab20'):
-    #df['lat'] = df['lat'].astype('float64')
-    #df['lon'] = df['lon'].astype('float64')
+def save_map(df, file, tiles='OpenStreetMap', label_id=dic_labels['id'], dic_labels = dic_labels, cmap='tab20'):
     m = folium.Map(tiles=tiles)
-    m.fit_bounds([ [df[trajutils.dic_labels['lat']].min(), df[trajutils.dic_labels['lon']].min()], [df[trajutils.dic_labels['lat']].max(), df[trajutils.dic_labels['lon']].max()] ])
+    m.fit_bounds([ [df[dic_labels['lat']].min(), df[dic_labels['lon']].min()], [df[dic_labels['lat']].max(), df[dic_labels['lon']].max()] ])
     
     ids = df[label_id].unique()
-    
-    #vals = np.linspace(0,1,256)
-    #np.random.seed(color_seed)
-    #np.random.shuffle(vals)
-    #cmap_ = plt.cm.colors.ListedColormap(plt.cm.hsv(vals))
-    # cmap_ = plt.cm.get_cmap( base_cmap, lut=ids.shape[0] )
     cmap_ = plt.cm.get_cmap( cmap )
     N = cmap_.N
     
     for id_ in ids:
         id_index = np.where(ids==id_)[0][0]
         df_ = df[ df[label_id] == id_ ]
-        #print('id:{}, shape:{}'.format(id_, df_.shape))
-        points_ = [ (point[0], point[1]) for point in df_[[trajutils.dic_labels['lat'], trajutils.dic_labels['lon']]].values]
+        points_ = [ (point[0], point[1]) for point in df_[[dic_labels['lat'], dic_labels['lon']]].values]
         color_ = cmap_hex_color(cmap_, (id_index % N))
-        #print( 'id_index:{}, color:{}'.format(id_index, color_) )
         folium.PolyLine(points_, weight=3, color=color_).add_to(m)
     m.save(file) 
   
-def save_wkt(df, file_str, label_id=trajutils.dic_labels['id'], dic_labels=trajutils.dic_labels):
+def save_wkt(df, file_str, label_id=dic_labels['id'], dic_labels=dic_labels):
     str_ = '{};linestring\n'.format(label_id)
     ids = df[label_id].unique()
     for id_ in ids:
@@ -72,23 +100,23 @@ def invert_map(map_):
 
 def show_object_id_by_date(df_, create_features=True, figsize=(21,9), save_fig=True, name='shot_points_by_date.png', low_memory=True):
     fig, ax = plt.subplots(2,2,figsize=figsize)
-    trajutils.create_update_date_features(df_)
+    create_update_date_features(df_)
     df_.loc[:,['date', 'id']].groupby(['id', 'date']).count().reset_index().groupby('date').count().plot(subplots=True, kind = 'line', grid=True, ax=ax[1][0], rot=45, fontsize=12)
-    trajutils.create_update_hour_features(df_)
+    create_update_hour_features(df_)
     df_.loc[:,['hour', 'id']].groupby(['hour', 'id']).count().reset_index().groupby('hour').count().plot(subplots=True, kind = 'line', grid=True, ax=ax[1][1], fontsize=12)
     del df_['date']    
     del df_['hour']
-    trajutils.create_update_day_of_the_week_features(df_)
+    create_update_day_of_the_week_features(df_)
     df_.loc[:,['period', 'id']].groupby(['period', 'id']).count().reset_index().groupby('period').count().plot(subplots=True, kind = 'bar', rot=0, ax=ax[0][0], fontsize=12)
     del df_['period']
-    trajutils.create_update_time_of_day_features(df_)  
+    create_update_time_of_day_features(df_)  
     df_.loc[:,['day', 'id']].groupby(['day', 'id']).count().reset_index().groupby('day').count().plot(subplots=True,  kind = 'bar', ax=ax[0][1], rot=0, fontsize=12)
     del df_['day']
 
     if save_fig:
         plt.savefig(fname=name, fig=fig)
 
-def show_lat_lon_GPS(df_, dic_labels=trajutils.dic_labels, kind='scatter', figsize=(21,9), save_fig=False, name='show_gps_points.png'):
+def show_lat_lon_GPS(df_, dic_labels=dic_labels, kind='scatter', figsize=(21,9), save_fig=False, name='show_gps_points.png'):
     try:
         if dic_labels['lat'] in df_ and dic_labels['lon'] in df_:
             df_.drop_duplicates([dic_labels['lat'], dic_labels['lon']]).plot(kind=kind, x=dic_labels['lon'], y=dic_labels['lat'], figsize=figsize)
@@ -117,7 +145,7 @@ def show_all_features(df_, figsize=(21,15), dtype=np.float64, save_fig=True, nam
     except Exception as e:
         raise e
 
-def show_traj(df, label_tid=trajutils.dic_labels['id'], dic_labels=trajutils.dic_labels, figsize=(10,10), return_fig=True, markers= 'o',markersize=20):
+def show_traj(df, label_tid=dic_labels['id'], dic_labels=dic_labels, figsize=(10,10), return_fig=True, markers= 'o',markersize=20):
     fig = plt.figure(figsize=figsize)
     ids = df[label_tid].unique()
     
@@ -127,7 +155,7 @@ def show_traj(df, label_tid=trajutils.dic_labels['id'], dic_labels=trajutils.dic
     if return_fig:
         return fig
 
-def show_traj_id(df, tid, label_tid=trajutils.dic_features_label['tid'], dic_labels=trajutils.dic_labels, figsize=(10,10)):
+def show_traj_id(df, tid, label_tid=dic_features_label['tid'], dic_labels=dic_labels, figsize=(10,10)):
     fig = plt.figure(figsize=figsize)
     df_ = df[ df[label_tid] == tid ]
     plt.plot(df_.iloc[0][dic_labels['lon']], df_.iloc[0][dic_labels['lat']], 'yo', markersize=20)             # start point
