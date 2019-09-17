e72336c697ba31de612ef82e23c279d6b7c5c6e6
Arina Sanches <arina@Insights-Mac-mini.local>
2019-09-17 15:22:34 -0300

    Continuação da estruturação do pymove, definição da interface.

---
+++ pymove/core/DaskMoveDataFrame.py
@@ -0,0 +1,3 @@
+import dask
+
+class PandasMoveDataFrame(MoveDataFrame, dask): # dask sua estrutura de dados
--- pymove/core/MoveDataFrame.py
+++ pymove/core/MoveDataFrame.py
@@ -1,84 +1,142 @@
-class MoveDataFrame():
+import abc
 
-    def __init__(self):
-        #....
-    
+class MoveDataFrame(abc.ABC):
+
+    #ddza
+    @abc.abstractmethod
     def head():
-        #....
-    
-    def show_trajectories_info(self):
-        #....
-    
+        pass
+
+    #ddza
+    @abc.abstractmethod   
     def read_file():
-        #....
+        pass
 
-    def write_file(self):
-        #....
-    
-    def plot_all_features():
-        #....
-    
-    def plot_trajs(self):
-        #....
-    
-    def plot_traj_id():
-        #....
-    
-    def len():
-        #....
-    
+    #ddza
+    @abc.abstractmethod
     def get_user_number(self):
-        #....
-    
+        pass
+
+    #ddza
+    @abc.abstractmethod
     def time_interval():
-        #....
-    
-    def get_bbox():
-        #....
-    
-    def __getattr__(self):
-        #....
+        pass  
 
-    def __setattr__(self):
-        #....
-
+    #ddza
+    @abc.abstractmethod
     def to_csv():
-        #...
+        pass
 
+    #ddza
+    @abc.abstractmethod
     def to_numpy():
-        #...  
+        pass          
 
+    #arina
+    @abc.abstractmethod   
+    def write_file(self):
+        pass
+
+    #arina
+    @abc.abstractmethod
+    def len():
+        pass
+
+    #arina
+    @abc.abstractmethod
+    def __getattr__(self):
+        pass
+
+    #arina
+    @abc.abstractmethod
+    def __setattr__(self):
+        pass
+
+    #arina
+    @abc.abstractmethod
     def to_dict():
-        #...  
+        pass
 
+    #arina
+    @abc.abstractmethod
     def to_grid():
-        #...
+        pass
 
+
+#####################
+
+    # Primeiros 7 andreza, os outros 7 arina
+    @abc.abstractmethod
     def with_tid_based_on_id_datatime():
-        #...    
+        pass
 
+    @abc.abstractmethod
     def with_date_features():
-        #...
-    
+        pass
+
+    @abc.abstractmethod
     def with_hour_features():
-        #...
+        pass
 
+    @abc.abstractmethod
     def with_day_of_the_week_features():
-        #...
+        pass
 
+    @abc.abstractmethod
     def with_time_of_day_features():
-        #...
+        pass
 
+    @abc.abstractmethod
     def with_dist_features():
-        #...
-    
+        pass
+
+    @abc.abstractmethod
     def with_dist_time_speed_features():
-        #...
+        pass
 
+    @abc.abstractmethod
     def with_move_and_stop_by_radius():
-        #...
+        pass
 
+    @abc.abstractmethod
     def time_interval():
-        #....
+        pass
     
+        @abc.abstractmethod
+    def get_bbox():
+        pass
 
+    @abc.abstractmethod   
+    def plot_all_features():
+        pass
+
+    @abc.abstractmethod   
+    def plot_trajs(self):
+        pass
+
+    @abc.abstractmethod
+    def plot_traj_id():
+        pass
+
+    @abc.abstractmethod
+    def show_trajectories_info(self):
+        pass
+
+#####################
+
+    #arina
+    #@property
+    def lat(self):
+        pass
+
+    #arina
+    #@property
+    def lng(self):
+        pass
+
+    #ddza
+    #@property
+    def datetime(self):
+        pass
+
+    
