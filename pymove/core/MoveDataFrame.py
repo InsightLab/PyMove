3be62c19d03f6a84a8a6f691a9869167e6f344f6
andyfernandes <andrezafernandes@alu.ufc.br>
2019-09-19 13:17:34 -0300

    [+] PandasMoveDataFrame

--- pymove/core/MoveDataFrame.py
+++ pymove/core/MoveDataFrame.py
@@ -7,136 +7,136 @@ class MoveDataFrame(abc.ABC):
     def head():
         pass
 
-    #ddza
-    @abc.abstractmethod   
-    def read_file():
-        pass
+#     #ddza
+#     @abc.abstractmethod   
+#     def read_file():
+#         pass
 
-    #ddza
-    @abc.abstractmethod
-    def get_user_number(self):
-        pass
+#     #ddza
+#     @abc.abstractmethod
+#     def get_user_number(self):
+#         pass
 
-    #ddza
-    @abc.abstractmethod
-    def time_interval():
-        pass  
+#     #ddza
+#     @abc.abstractmethod
+#     def time_interval():
+#         pass  
 
-    #ddza
-    @abc.abstractmethod
-    def to_csv():
-        pass
+#     #ddza
+#     @abc.abstractmethod
+#     def to_csv():
+#         pass
 
-    #ddza
-    @abc.abstractmethod
-    def to_numpy():
-        pass          
+#     #ddza
+#     @abc.abstractmethod
+#     def to_numpy():
+#         pass          
 
-    #arina
-    @abc.abstractmethod   
-    def write_file(self):
-        pass
+#     #arina
+#     @abc.abstractmethod   
+#     def write_file(self):
+#         pass
 
-    #arina
-    @abc.abstractmethod
-    def len():
-        pass
+#     #arina
+#     @abc.abstractmethod
+#     def len():
+#         pass
 
-    #arina
-    @abc.abstractmethod
-    def __getattr__(self):
-        pass
+#     #arina
+#     @abc.abstractmethod
+#     def __getattr__(self):
+#         pass
 
-    #arina
-    @abc.abstractmethod
-    def __setattr__(self):
-        pass
+#     #arina
+#     @abc.abstractmethod
+#     def __setattr__(self):
+#         pass
 
-    #arina
-    @abc.abstractmethod
-    def to_dict():
-        pass
+#     #arina
+#     @abc.abstractmethod
+#     def to_dict():
+#         pass
 
-    #arina
-    @abc.abstractmethod
-    def to_grid():
-        pass
+#     #arina
+#     @abc.abstractmethod
+#     def to_grid():
+#         pass
 
 
-#####################
+# #####################
 
-    # Primeiros 7 andreza, os outros 7 arina
-    @abc.abstractmethod
-    def with_tid_based_on_id_datatime():
-        pass
+#     # Primeiros 7 andreza, os outros 7 arina
+#     @abc.abstractmethod
+#     def with_tid_based_on_id_datatime():
+#         pass
 
-    @abc.abstractmethod
-    def with_date_features():
-        pass
+#     @abc.abstractmethod
+#     def with_date_features():
+#         pass
 
-    @abc.abstractmethod
-    def with_hour_features():
-        pass
+#     @abc.abstractmethod
+#     def with_hour_features():
+#         pass
 
-    @abc.abstractmethod
-    def with_day_of_the_week_features():
-        pass
+#     @abc.abstractmethod
+#     def with_day_of_the_week_features():
+#         pass
 
-    @abc.abstractmethod
-    def with_time_of_day_features():
-        pass
+#     @abc.abstractmethod
+#     def with_time_of_day_features():
+#         pass
 
-    @abc.abstractmethod
-    def with_dist_features():
-        pass
+#     @abc.abstractmethod
+#     def with_dist_features():
+#         pass
 
-    @abc.abstractmethod
-    def with_dist_time_speed_features():
-        pass
+#     @abc.abstractmethod
+#     def with_dist_time_speed_features():
+#         pass
 
-    @abc.abstractmethod
-    def with_move_and_stop_by_radius():
-        pass
+#     @abc.abstractmethod
+#     def with_move_and_stop_by_radius():
+#         pass
 
-    @abc.abstractmethod
-    def time_interval():
-        pass
+#     @abc.abstractmethod
+#     def time_interval():
+#         pass
     
-        @abc.abstractmethod
-    def get_bbox():
-        pass
+#         @abc.abstractmethod
+#     def get_bbox():
+#         pass
 
-    @abc.abstractmethod   
-    def plot_all_features():
-        pass
+#     @abc.abstractmethod   
+#     def plot_all_features():
+#         pass
 
-    @abc.abstractmethod   
-    def plot_trajs(self):
-        pass
+#     @abc.abstractmethod   
+#     def plot_trajs(self):
+#         pass
 
-    @abc.abstractmethod
-    def plot_traj_id():
-        pass
+#     @abc.abstractmethod
+#     def plot_traj_id():
+#         pass
 
-    @abc.abstractmethod
-    def show_trajectories_info(self):
-        pass
+#     @abc.abstractmethod
+#     def show_trajectories_info(self):
+#         pass
 
-#####################
+# #####################
 
-    #arina
-    #@property
-    def lat(self):
-        pass
+#     #arina
+#     #@property
+#     def lat(self):
+#         pass
 
-    #arina
-    #@property
-    def lng(self):
-        pass
+#     #arina
+#     #@property
+#     def lng(self):
+#         pass
 
-    #ddza
-    #@property
-    def datetime(self):
-        pass
+#     #ddza
+#     #@property
+#     def datetime(self):
+#         pass
 
     
