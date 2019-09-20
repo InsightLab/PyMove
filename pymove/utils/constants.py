0712697c4438a0cb39f936b368f8cc9af10b502b
ArinaSanches <sanchesarina96@gmail.com>
2019-09-20 17:54:07 -0300

    adição do arquivo de constantes

---
+++ pymove/utils/constants.py
@@ -0,0 +1,22 @@
+
+LATITUDE = 'lat'
+LONGITUDE = 'lon'
+DATETIME = 'datetime'
+TRAJ_ID = 'id'
+TID = "tid"
+
+DIST_TO_PREV = 'dist_to_prev'
+DIST_TO_NEXT = 'dist_to_next'
+DIST_PREV_TO_NEXT = 'dist_prev_to_next'
+TIME_TO_PREV = 'time_to_prev'
+TIME_TO_NEXT = 'time_to_next'
+SPEED_TO_PREV = 'speed_to_prev'
+SPEED_TO_NEXT = 'speed_to_next'
+PERIOD = "period"
+DAY = "day"
+INDEX_GRID_LAT = 'index_grid_lat'
+INDEX_GRID_LON = 'index_grid_lon'
+SITUATION = 'situation'
+
+
+
