6a2d9155d56e4ab1f6beabe98d89694e69715656
ArinaSanches <sanchesarina96@gmail.com>
2019-10-05 23:24:37 -0300

    Mudança do Pandas e do Dask para nova esstrutura

--- pymove/core/MoveDataFrame.py
+++ pymove/core/MoveDataFrame.py
@@ -1,15 +1,16 @@
 import abc
 from pymove.utils.traj_utils import format_labels
 from pymove.core.PandasMoveDataFrame import PandasMoveDataFrame as pm
+from pymove.core.DaskMoveDataFrame import DaskMoveDataFrame as dm
 from pymove.utils.constants import LATITUDE, LONGITUDE, DATETIME, TRAJ_ID
- 
+
+
 class MoveDataFrame():
     @staticmethod
-    def __new__(self, data, latitude=LATITUDE, longitude=LONGITUDE, datetime=DATETIME, traj_id = TRAJ_ID, type="pandas"):
+    def __new__(self, data, latitude=LATITUDE, longitude=LONGITUDE, datetime=DATETIME, traj_id=TRAJ_ID, type="pandas",
+                n_partitions=1):
         self.type = type
-        if type == 'pandas':            
-            return pm(data, latitude=LATITUDE, longitude=LONGITUDE, datetime=DATETIME, traj_id = TRAJ_ID)
-
-
-
-    
+        if type == 'pandas':
+            return pm(data, latitude=LATITUDE, longitude=LONGITUDE, datetime=DATETIME, traj_id=TRAJ_ID)
+        if type == 'dask':
+            return dm(data, latitude=LATITUDE, longitude=LONGITUDE, datetime=DATETIME, traj_id=TRAJ_ID, n_partitions=1)
