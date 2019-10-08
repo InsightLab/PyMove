e474372d307b552960b99ecb6c19bfe28fdef48b
Arina Sanches <arina@Insights-Mac-mini.local>
2019-10-08 14:51:43 -0300

    Tudo das issues 3 e 4  funcionando,  menos o set_type()

--- pymove/core/DaskMoveDataFrame.py
+++ pymove/core/DaskMoveDataFrame.py
@@ -23,8 +23,8 @@ from pymove.core import MoveDataFrameAbstractModel
 class DaskMoveDataFrame(DataFrame, MoveDataFrameAbstractModel):  # dask sua estrutura de dados
 	def __init__(self, data, latitude=LATITUDE, longitude=LONGITUDE, datetime=DATETIME, traj_id=TRAJ_ID,
 				 n_partitions=1):
-		# formatar os labels que foram passados pro que usado no pymove -> format_labels
-		# renomeia as colunas do dado passado pelo novo dict
+		# formatar os labels que foram return 0ados pro que usado no pymove -> format_labels
+		# renomeia as colunas do dado return 0ado pelo novo dict
 		# cria o dataframe
 		mapping_columns = format_labels(data, traj_id, latitude, longitude, datetime)
 		dsk = data.rename(columns=mapping_columns)
@@ -95,3 +95,153 @@ class DaskMoveDataFrame(DataFrame, MoveDataFrameAbstractModel):  # dask sua estr
 	def get_type(self):
 		return self._type
 
+	 
+	def get_users_number(self):
+	    return 0
+	
+	 
+	def time_interval(self):
+	    return 0
+	
+	 
+	def to_numpy(self):
+	    return 0
+	
+	 
+	def write_file(self):
+	    return 0
+	
+	 
+	def len(self):
+	    return 0
+	
+
+	 
+	def to_dict(self):
+	    return 0
+	
+	 
+	def to_grid(self):
+	    return 0
+	
+	 
+	def generate_tid_based_on_id_datatime(self):
+	    return 0
+	
+	 
+	def generate_date_features(self):
+	    return 0
+	
+	 
+	def generate_hour_features(self):
+	    return 0
+	
+	 
+	def generate_day_of_the_week_features(self):
+	    return 0
+	
+	 
+	def generate_time_of_day_features(self):
+	    return 0
+	
+	 
+	def generate_dist_features(self):
+	    return 0
+	
+	 
+	def generate_dist_time_speed_features(self):
+	    return 0
+	
+	 
+	def generate_move_and_stop_by_radius(self):
+	    return 0
+	
+	 
+	def time_interval(self):
+	    return 0
+	
+	 
+	def get_bbox(self):
+	    return 0
+	
+	 
+	def plot_all_features(self):
+	    return 0
+	
+	 
+	def plot_trajs(self):
+	    return 0
+	
+	 
+	def plot_traj_id():
+	    return 0
+	
+	 
+	def show_trajectories_info(self):
+	    return 0
+	
+	def count(self):
+	    return 0
+	
+
+	def reset_index(self):
+	    return 0
+	
+
+	def plot(self):
+	    return 0
+	
+
+	def drop_duplicates(self):
+	    return 0
+	
+
+	def select_dtypes(self):
+	    return 0
+	
+	def sort_values(self):
+	    return 0
+	
+	 
+	def astype(self):
+	    return 0
+	
+	 
+	def set_index(self):
+	    return 0
+	
+	 
+	def drop(self):
+	    return 0
+	
+	 
+	def duplicated(self):
+	    return 0
+	
+	 
+	def shift(self):
+	    return 0
+	
+	 
+	def any(self):
+	    return 0
+	
+	 
+	def dropna(self):
+	    return 0
+	
+	 
+	def isin(self):
+	    return 0
+	
+	 
+	def append(self):
+	    return 0
+	
+	 
+	def nunique(self):
+	    return 0
+	
+	 
+	def to_csv(self):
+	    return 0
