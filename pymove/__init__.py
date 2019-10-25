a720628d2388e318fedb0d40fcb456b675af1d43
ArinaSanches <sanchesarina96@gmail.com>
2019-10-25 17:01:45 -0300

    Comentario de transformation

--- pymove/__init__.py
+++ pymove/__init__.py
@@ -1,19 +1,19 @@
-from pymove.utils import trajectories
-from pymove.utils import constants
-from pymove.utils import transformations
-from pymove.utils import conversions
+# from pymove.utils import trajectories
+# from pymove.utils import constants
+# from pymove.utils import transformations
+# from pymove.utils import conversions
 from pymove.utils import datetime
-from pymove.utils import distances
-from pymove.utils import math
-from pymove.preprocessing import filters
-from pymove.preprocessing import map_matching
-from pymove.preprocessing import segmentation
-from pymove.utils.trajectories import (read_csv)
-from pymove.core.dataframe import MoveDataFrame
-from pymove.core.dataframe import PandasMoveDataFrame
-from pymove.core.dataframe import DaskMoveDataFrame
-from pymove.core import grid
-
-
+# from pymove.utils import distances
+# from pymove.utils import math
+# from pymove.preprocessing import filters
+# from pymove.preprocessing import map_matching
+# from pymove.preprocessing import segmentation
+# from pymove.utils.trajectories import (read_csv)
+# from pymove.core.dataframe import MoveDataFrame
+# from pymove.core.dataframe import PandasMoveDataFrame
+# from pymove.core.dataframe import DaskMoveDataFrame
+# from pymove.core import grid
+#
+#
 
 
