fc16bf1838ed49465dc328b84c326baaeee8f57b
andrezafernandes <andrezafernandes@insight-labs-Mac-mini.local>
2019-11-19 17:29:30 -0300

    [+] Setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup
 
-dependencies = ['numpy', 'pandas', 'folium', 'matplotlib', 'sharpely', '']
+dependencies = ['numpy', 'pandas', 'folium', 'matplotlib', 'shapely', 'IPython', 'ipywidgets',
+'scipy', 'resource', 'dask', 'tqdm']
 
 setup(name='pymove',
       version='0.1',
-      description='A lib python to ',
-      url='http://github.com/storborg/funniest',
-      author='Flying Circus',
-      author_email='flyingcircus@example.com',
+      description='A lib python to processing and visualization of trajectories and other spatial-temporal data',
+      url='https://github.com/InsightLab/PyMove',
+      author='Insight Data Science Lab',
+      author_email='insightlab@dc.ufc.br',
       license='MIT',
-      packages=['funniest'],
+      packages=['pymove'],
       install_requires=dependencies,
       zip_safe=False)
