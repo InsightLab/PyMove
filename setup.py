6804b8f69f9e8452785fd3a4a505d860681da89f
ArinaSanches <sanchesarina96@gmail.com>
2020-02-10 16:56:05 -0300

    Correção set-up

--- setup.py
+++ setup.py
@@ -1,15 +1,33 @@
-from setuptools import setup
+try:
+      from setuptools import setup, find_packages
+except ImportError:
+      from distutils.core import setup, find_packages
 
-dependencies = ['numpy', 'pandas', 'folium', 'matplotlib', 'shapely', 'IPython', 'ipywidgets',
-'scipy', 'resource', 'dask', 'tqdm']
+# Get the long description from the README file
+try:
+      long_description = open('README.md', "r").read()
+except Exception as e:
+      raise e
 
-setup(name='pymove',
-      version='0.1',
-      description='A lib python to processing and visualization of trajectories and other spatial-temporal data',
-      url='https://github.com/InsightLab/PyMove',
-      author='Insight Data Science Lab',
+DEPENDENCIES = ['tqdm', 'numpy', 'pandas', 'scipy', 'geojson', 'matplotlib', 'shapely', 'folium', 'mplleaflet',
+                ,'matplotlib', 'IPython', 'psutil', 'ipywidgets', 'resource', 'dask']
+
+setup(
+      name="pymove",
+      version="0.1",
+      author="Insight Data Science Lab",
       author_email='insightlab@dc.ufc.br',
       license='MIT',
+      python_requires='>=3.6',
+      description='A lib python to processing and visualization of trajectories and other spatial-temporal data',
+      long_description=long_description,
+      url="https://github.com/InsightLab/PyMove",
       packages=['pymove'],
-      install_requires=dependencies,
-      zip_safe=False)
+      classifiers=[
+            'Development Status :: 4 - Beta',
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+      ],
+      install_requires=DEPENDENCIES,
+)
