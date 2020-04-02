819b960b9b600915f973f3ca4750a1cb49e9b7b7
Mauricio Cavalcante <mcavalcante16@github.com>
2020-04-02 12:35:32 -0300

    Corrige Documentação de _create_virtual_grid e create_all_polygons_to_all_point_on_grid
    Former-commit-id: 2ae65a294a5681c7e595d26bf72ae244d9bf433e

--- pymove/core/grid.py
+++ pymove/core/grid.py
@@ -28,11 +28,11 @@ class Grid():
 
         Parameters
         ----------
+        data : pandas.core.frame.DataFrame
+            Represents the dataset with contains lat, long and datetime.
+                
         cell_size : float
             Size of grid's cell.
-
-        bbox : tuple
-            Represents a bound box, that is a tuple of 4 values with the min and max limits of latitude e longitude.
 
         meters_by_degree : float
             Represents the meters's degree of latitude. By default the latitude is set in Fortaleza.
@@ -48,6 +48,8 @@ class Grid():
                 - cell_size_by_degree: grid's cell size.
 
         """
+        
+        """bbox: a bound box, that is a tuple of 4 values with the min and max limits of latitude e longitude."""
         bbox = data.get_bbox()
         print('\nCreating a virtual grid without polygons')
     
@@ -178,14 +180,6 @@ class Grid():
         ----------
         data : pandas.core.frame.DataFrame
             Represents the dataset with contains lat, long and datetime.
-
-        dic_grid : dict
-            Contains informations about virtual grid, how
-                - lon_min_x: longitude mínima.
-                - lat_min_y: latitude miníma. 
-                - grid_size_lat_y: tamanho da grid latitude. 
-                - grid_size_lon_x: tamanho da longitude da grid.
-                - cell_size_by_degree: tamanho da célula da Grid.
 
         Returns
         -------
