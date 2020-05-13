bf93c5ef5e3400f0b3cdc8e9e0809e0c1df6c657
flycher <xavier_sampaio@alu.ufc.br>
2020-05-13 13:30:07 -0300

    Bump version: 1.1.8 → 1.2.0

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ DEPENDENCIES = [
 
 setup(
     name="pymove",
-    version="1.1.8",
+    version="1.2.0",
     author="Insight Data Science Lab",
     author_email="insightlab@dc.ufc.br",
     license="MIT",
