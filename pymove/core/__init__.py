76b079a9fb0d1d29c15d2755164665841683ff27
flycher <xavier_sampaio@alu.ufc.br>
2020-05-16 10:04:06 -0300

    fix documentacao PandasMoveDataFrame

--- pymove/core/__init__.py
+++ pymove/core/__init__.py
@@ -1,3 +1,10 @@
+"""
+Contains the core of PyMove: MoveDataFrame, PandasMoveDataFrame, DaskMoveDataFrame and Grid.
+
+All functions and objects are available in the main ``pymove`` namespace.
+
+"""
+
 from .interface import MoveDataFrameAbstractModel
 
 __all__ = ["MoveDataFrameAbstractModel"]
