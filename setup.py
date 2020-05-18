622812b122b51a3f9cfbfef82941ee328fd2e257
flycher <xavier_sampaio@alu.ufc.br>
2020-05-18 00:18:54 -0300

    Bump version: 1.2.0 → 1.3.0
    Former-commit-id: aea4931899e7b3d6deb32816fa89775db9f1d250

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ DEPENDENCIES = [
 
 setup(
     name='pymove',
-    version='1.2.0',
+    version='1.3.0',
     author='Insight Data Science Lab',
     author_email='insightlab@dc.ufc.br',
     license='MIT',
