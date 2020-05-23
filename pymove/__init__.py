fb28b52600828c0ac427b194d63c6172221ef172
flycher <xavier_sampaio@alu.ufc.br>
2020-05-22 21:59:53 -0300

    tests preprocessing
    Former-commit-id: 5f558e5158d47279972dbdae1608da991ab15578

--- pymove/__init__.py
+++ pymove/__init__.py
@@ -9,18 +9,9 @@ spatial-temporal data
 
 from ._version import __version__
 from .core import grid
-from .core.dataframe import (
-    DaskMoveDataFrame,
-    MoveDataFrame,
-    PandasMoveDataFrame,
-)
+from .core.dataframe import DaskMoveDataFrame, MoveDataFrame, PandasMoveDataFrame
 from .models.pattern_mining import clustering
-from .preprocessing import (
-    compression,
-    filters,
-    segmentation,
-    stay_point_detection,
-)
+from .preprocessing import compression, filters, segmentation, stay_point_detection
 from .semantic import semantic
 from .utils import (
     constants,
