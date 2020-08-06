9ee832a0221e7fc59a6cca39377e3e8b6bd170a7
Júnior Alves <pedroccufc@gmail.com>
2020-08-06 00:55:01 -0300

    add new constants
    Former-commit-id: 27a883c339c4ab3b6395255a6030b86afe9c5480

--- pymove/utils/constants.py
+++ pymove/utils/constants.py
@@ -5,6 +5,23 @@ TRAJ_ID = 'id'
 TID = 'tid'
 UID = 'user_id'
 
+GEOHASH = 'geohash'
+BIN_GEOHASH = 'bin_geohash'
+LATITUDE_DECODE = 'lat_d'
+LONGITUDE_DECODE = 'lon_d'
+
+COLOR = {
+    True: '#6699cc',
+    False: '#ffcc33'
+}
+
+BASE_32 = ['0', '1', '2', '3', '4', '5',
+           '6', '7', '8', '9', 'a', 'b',
+           'c', 'd', 'e', 'f', 'g', 'h',
+           'i', 'j', 'k', 'l', 'm', 'n',
+           'o', 'p', 'q', 'r', 's', 't',
+           'u', 'v', 'w', 'x', 'y', 'z']
+
 POI = 'poi'
 ID_POI = 'id_poi'
 DIST_POI = 'dist_poi'
