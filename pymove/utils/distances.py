df1dca064c76fff0c0bfd7bb51b7e353efc28cbd
Mauricio Cavalcante <mcavalcante16@github.com>
2020-09-28 18:06:27 -0300

    Adiciona constantes segundo o padrao e remove codigos duplicados
    Former-commit-id: 930c4fda1fa6c72de68812d23458639a3faa3251

--- pymove/utils/distances.py
+++ pymove/utils/distances.py
@@ -3,6 +3,7 @@ import pandas as pd
 from scipy.spatial import distance
 
 from pymove import utils
+from pymove.utils.constants import DATETIME, LATITUDE, LONGITUDE
 
 
 def haversine(lat1, lon1, lat2, lon2, to_radians=True, earth_radius=6371):
@@ -57,7 +58,7 @@ def haversine(lat1, lon1, lat2, lon2, to_radians=True, earth_radius=6371):
         raise e
 
 
-def _nearest_points(traj1, traj2, label_lat='lat', label_lon='lon'):
+def nearest_points(traj1, traj2, latitude=LATITUDE, longitude=LONGITUDE):
     """
     For each point on a trajectory, it returns the point closest to
     another trajectory based on the Euclidean distance.
@@ -70,22 +71,22 @@ def _nearest_points(traj1, traj2, label_lat='lat', label_lon='lon'):
     traj2: dataframe
         The input of another trajectory.
 
-    label_lat: string ("lat" by default)
+    latitude: string ("lat" by default)
         Label of the trajectories dataframe referring to the latitude.
 
-    label_lon: string ("lon" by default)
+    longitude: string ("lon" by default)
         Label of the trajectories dataframe referring to the longitude.
     """
 
     result = pd.DataFrame(columns=traj1.columns)
 
     for i in range(0, len(traj1)):
-        round_result = 999999999999999
+        round_result = np.Inf
         round_traj = []
         for j in range(0, len(traj2)):
             this_distance = distance.euclidean(
-                (traj1[label_lat].iloc[i], traj1[label_lon].iloc[i]),
-                (traj2[label_lat].iloc[j], traj2[label_lon].iloc[j]),
+                (traj1[latitude].iloc[i], traj1[longitude].iloc[i]),
+                (traj2[latitude].iloc[j], traj2[longitude].iloc[j]),
             )
             if this_distance < round_result:
                 round_result = this_distance
@@ -95,7 +96,7 @@ def _nearest_points(traj1, traj2, label_lat='lat', label_lon='lon'):
     return result
 
 
-def MEDP(traj1, traj2, label_lat='lat', label_lon='lon'):
+def MEDP(traj1, traj2, latitude=LATITUDE, longitude=LONGITUDE):
     """
     Returns the Mean Euclidian Distance Predictive between
     two trajectories, which considers only the spatial
@@ -109,26 +110,26 @@ def MEDP(traj1, traj2, label_lat='lat', label_lon='lon'):
     traj2: dataframe
         The input of another trajectory.
 
-    label_lat: string ("lat" by default)
+    latitude: string ("lat" by default)
         Label of the trajectories dataframe referring to the latitude.
 
-    label_lon: string ("lon" by default)
+    longitude: string ("lon" by default)
         Label of the trajectories dataframe referring to the longitude.
     """
 
     soma = 0
-    traj2 = _nearest_points(traj1, traj2, label_lat, label_lon)
+    traj2 = nearest_points(traj1, traj2, latitude, longitude)
     for i in range(0, len(traj1)):
         this_distance = distance.euclidean(
-            (traj1[label_lat].iloc[i],
-             traj1[label_lon].iloc[i]),
-            (traj2[label_lat].iloc[i],
-             traj2[label_lon].iloc[i]))
+            (traj1[latitude].iloc[i],
+             traj1[longitude].iloc[i]),
+            (traj2[latitude].iloc[i],
+             traj2[longitude].iloc[i]))
         soma = soma + this_distance
     return soma
 
 
-def MEDT(traj1, traj2, label_lat='lat', label_lon='lon', label_time='datetime'):
+def MEDT(traj1, traj2, latitude=LATITUDE, longitude=LONGITUDE, datetime=DATETIME):
     """
     Returns the Mean Euclidian Distance Trajectory between two
     trajectories, which considers the spatial dimension and the
@@ -142,54 +143,37 @@ def MEDT(traj1, traj2, label_lat='lat', label_lon='lon', label_time='datetime'):
     traj2: dataframe
         The input of another trajectory.
 
-    label_lat: string ("lat" by default)
+    latitude: string ("lat" by default)
         Label of the trajectories dataframe referring to the latitude.
 
-    label_lon: string ("lon" by default)
+    longitude: string ("lon" by default)
         Label of the trajectories dataframe referring to the longitude.
 
-    label_time: string ("datetime" by default)
+    datetime: string ("datetime" by default)
         Label of the trajectories dataframe referring to the timestamp.
     """
 
     soma = 0
-    if(len(traj2) > len(traj1)):
-        for i in range(0, len(traj1)):
-            this_distance = distance.euclidean(
-                (traj1[label_lat].iloc[i],
-                 traj1[label_lon].iloc[i],
-                 float(utils.datetime.timestamp_to_millis(
-                     traj1[label_time].iloc[i]
-                 )) / 1000000000),
-                (traj2[label_lat].iloc[i],
-                 traj2[label_lon].iloc[i],
-                 float(utils.datetime.timestamp_to_millis(
-                     traj2[label_time].iloc[i]
-                 )) / 1000000000),
-            )
-            soma = soma + this_distance
-        for j in range(len(traj1) + 1, len(traj2)):
-            soma = soma + \
-                float(utils.datetime.timestamp_to_millis(
-                    traj2[label_time].iloc[j])) / 1000000000
+    proportion = 1000000000
+    if(len(traj2) < len(traj1)):
+        traj1, traj2 = traj2, traj1
 
-    elif(len(traj1) > len(traj2)):
-        for i in range(0, len(traj2)):
-            this_distance = distance.euclidean(
-                (traj1[label_lat].iloc[i],
-                 traj1[label_lon].iloc[i],
-                 float(utils.datetime.timestamp_to_millis(
-                     traj1[label_time].iloc[i]
-                 )) / 1000000000),
-                (traj2[label_lat].iloc[i],
-                 traj2[label_lon].iloc[i],
-                 float(utils.datetime.timestamp_to_millis(
-                     traj2[label_time].iloc[i]
-                 )) / 1000000000),
-            )
-            soma = soma + this_distance
-        for j in range(len(traj2) + 1, len(traj1)):
-            soma = soma + \
+    for i in range(0, len(traj1)):
+        this_distance = distance.euclidean(
+            (traj1[latitude].iloc[i],
+                traj1[longitude].iloc[i],
                 float(utils.datetime.timestamp_to_millis(
-                    traj1[label_time].iloc[j])) / 1000000000
+                    traj1[datetime].iloc[i]
+                )) / proportion),
+            (traj2[latitude].iloc[i],
+                traj2[longitude].iloc[i],
+                float(utils.datetime.timestamp_to_millis(
+                    traj2[datetime].iloc[i]
+                )) / proportion),
+        )
+        soma = soma + this_distance
+    for j in range(len(traj1) + 1, len(traj2)):
+        soma = soma + \
+            float(utils.datetime.timestamp_to_millis(
+                traj2[datetime].iloc[j])) / proportion
     return soma
