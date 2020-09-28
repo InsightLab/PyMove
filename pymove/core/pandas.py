2484e867056c9e0dc35115dcdb8eb113fc213251
Pedro Alves <pedroccufc@gmail.com>
2020-09-28 17:42:54 -0300

    add method to_discrete_move_df

--- pymove/core/pandas.py
+++ pymove/core/pandas.py
@@ -13,11 +13,9 @@ from pymove.utils.constants import (
     DIST_PREV_TO_NEXT,
     DIST_TO_NEXT,
     DIST_TO_PREV,
-    GRID_ID,
     HOUR,
     HOUR_COS,
     HOUR_SIN,
-    INDEX_GRID,
     LATITUDE,
     LOCAL_LABEL,
     LONGITUDE,
@@ -642,18 +640,20 @@ class PandasMoveDataFrame(pd.DataFrame, MoveDataFrameAbstractModel):
             Represents an PandasMoveDataFrame dicretized.
         """
 
-        try:
-            data_ = self._data
-            if local_label not in data_:
-                raise ValueError(
-                    'columns {} not in df'.format(local_label)
-                )
-            return PandasDiscreteMoveDataFrame(
-                data_, LATITUDE, LONGITUDE, DATETIME, TRAJ_ID, local_label
+        operation = begin_operation('to_discrete_move_df')
+        data_ = self._data
+
+        if local_label not in data_:
+            raise ValueError(
+                'columns {} not in df'.format(local_label)
             )
 
-        except Exception as e:
-            raise e
+        self.last_operation = end_operation(operation)
+
+        from pymove.core.pandas_discrete import PandasDiscreteMoveDataFrame
+        return PandasDiscreteMoveDataFrame(
+            data_, LATITUDE, LONGITUDE, DATETIME, TRAJ_ID, local_label
+        )
 
     def info(
             self,
@@ -3466,107 +3466,3 @@ class PandasMoveDataFrame(pd.DataFrame, MoveDataFrameAbstractModel):
         type_ = self._type
         self.last_operation = end_operation(operation)
         return type_
-
-
-class PandasDiscreteMoveDataFrame(PandasMoveDataFrame):
-    def __init__(
-        self,
-        data,
-        latitude=LATITUDE,
-        longitude=LONGITUDE,
-        datetime=DATETIME,
-        traj_id=TRAJ_ID,
-        local_label=LOCAL_LABEL
-    ):
-        """
-        Checks whether past data has 'lat', 'lon', 'datetime' and 'local_label'
-        columns, and renames it with the PyMove lib standard. After starts the
-        attributes of the class.
-
-        - self._data : Represents trajectory data.
-        - self._type : Represents the type of layer below the data structure.
-        - self.last_operation : Represents the last operation perfomed.
-
-        Parameters
-        ----------
-        data : dict, list, numpy array or pandas.core.DataFrame
-            Input trajectory data.
-        latitude : str, optional, default 'lat'.
-            Represents column name latitude.
-        longitude : str, optional, default 'lon'.
-            Represents column name longitude.
-        datetime : str, optional, default 'datetime'.
-            Represents column name datetime.
-        traj_id : str, optional, default 'id'.
-            Represents column name trajectory id.
-        local_label : str, optional, default 'local_label'.
-            Represents column name local label
-
-        Raises
-        ------
-        KeyError
-            If missing one of lat, lon, datetime, local_label columns
-        ValueError, ParserError
-            If the data types can't be converted.
-
-        """
-
-        super()
-
-        if isinstance(data, dict):
-            data = pd.DataFrame.from_dict(data)
-        elif (
-            (isinstance(data, list) or isinstance(data, np.ndarray))
-            and len(data) >= 5
-        ):
-            zip_list = [LATITUDE, LONGITUDE, DATETIME, TRAJ_ID, local_label]
-            for i in range(len(data[0])):
-                try:
-                    zip_list[i] = zip_list[i]
-                except KeyError:
-                    zip_list.append(i)
-            data = pd.DataFrame(data, columns=zip_list)
-
-        mapping_columns = MoveDataFrame.format_labels(
-            traj_id, latitude, longitude, datetime
-        )
-        tdf = data.rename(columns=mapping_columns)
-
-        if local_label not in tdf:
-            raise ValueError(
-                '{} column not in dataframe'.format(local_label)
-            )
-
-        if MoveDataFrame.has_columns(tdf):
-            MoveDataFrame.validate_move_data_frame(tdf)
-            self._data = tdf
-            self._type = TYPE_PANDAS
-            self.last_operation = None
-        else:
-            raise AttributeError(
-                'Couldn\'t instantiate MoveDataFrame because data has missing columns'
-            )
-
-    def discretize_based_grid(self, region_size=1000):
-        """
-        Discrete space in cells of the same size,
-        assigning a unique id to each cell.
-
-        Parameters
-        ----------
-        region_size: number, optional, default 1000
-            Size of grid'srs cell.
-        """
-
-        operation = begin_operation('discretize based on grid')
-        print('\nDiscretizing dataframe...')
-        try:
-            grid = Grid(self, cell_size=region_size)
-            grid.create_update_index_grid_feature(self)
-            self.rename(columns={INDEX_GRID: GRID_ID}, inplace=True)
-            self.reset_index(drop=True, inplace=True)
-            self.last_operation = end_operation(operation)
-
-        except Exception as e:
-            self.last_operation = end_operation(operation)
-            raise e
