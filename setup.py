807ae3ce7f5491863d45e2aa333c46d3fa47b5b9
flych3r <xavier_sampaio@alu.ufc.br>
2021-01-31 18:00:30 -0300

    Bump version: 2.4.0 → 2.5.0

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ DEPENDENCIES = [
 ]
 setup(
     name='pymove',
-    version='2.4.0',
+    version='2.5.0',
     author='Insight Data Science Lab',
     author_email='insightlab@dc.ufc.br',
     license='MIT',
