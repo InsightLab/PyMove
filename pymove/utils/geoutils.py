bacf35163d22e4e85c84eb9b7c95320778824062
João Victor <joaocarvoli@hotmail.com>
2021-05-19 10:37:28 -0300

    putting examples on geoutils module

--- pymove/utils/geoutils.py
+++ pymove/utils/geoutils.py
@@ -56,6 +56,20 @@ def v_color(ob: BaseGeometry) -> Text:
     ------
     str
         Geometric object color
+
+    Example
+    -------
+    >>> from pymove.utils.geoutils import v_color
+    >>> from shapely.geometry import LineString
+    >>> case_1 = LineString([(3,3),(4,4), (3,4)])
+    >>> case_2 = LineString([(3,3),(4,4), (4,3)])
+    >>> case_3 = LineString([(3,3),(4,4), (3,4), (4,3)])
+    >>> print(v_color(case_1), type(v_color(case_1)))
+    #6699cc <class 'str'>
+    >>> print(v_color(case_2), type(v_color(case_2)))
+    #6699cc <class 'str'>
+    >>> print(v_color(case_3), type(v_color(case_3)))
+    #ffcc33 <class 'str'>
     """
     return COLORS[ob.is_simple + 33]
 
@@ -72,6 +86,9 @@ def plot_coords(ax: axes, ob: BaseGeometry, color: Optional[Text] = 'r'):
         Any geometric object
     color : str, optional
         Sets the geometric object color, by default 'r'
+
+    Example
+    -------
     """
     x, y = ob.xy
     ax.plot(x, y, 'o', color=color, zorder=1)
@@ -89,6 +106,10 @@ def plot_bounds(ax: axes, ob: Union[LineString, MultiLineString], color='b'):
         Geometric object formed by lines.
     color : str, optional
         Sets the geometric object color, by default 'b'
+
+    Example
+    -------
+
     """
     x, y = zip(*list((p.x, p.y) for p in ob.boundary))
     ax.plot(x, y, '-', color=color, zorder=1)
@@ -122,6 +143,9 @@ def plot_line(
         Defines the style of the ends of the line, by default 'round'
     zorder : float, optional
         Determines the default drawing order for the axes, by default 2
+
+    Example
+    -------
     """
     x, y = ob.xy
     ax.plot(
@@ -149,6 +173,16 @@ def _encode(lat: float, lon: float, precision: Optional[float] = 15) -> Text:
     ------
     str
         Geohash of supplied latitude/longitude.
+
+    Example
+    -------
+    >>> from pymove.utils.geoutils import _encode
+    >>> lat1, lon1 = -3.777736, -38.547792
+    >>> lat2, lon2 = -3.793388, -38.517722
+    >>> print(_encode(lat1,lon1), type(_encode(lat1,lon1)))
+    7pkddb6356fyzxq <class 'str'>
+    >>> print(_encode(lat2,lon2), type(_encode(lat2,lon2)))
+    7pkd7t2mbj0z1v7 <class 'str'>
     """
     return gh.encode(lat, lon, precision)
 
@@ -168,6 +202,16 @@ def _decode(geohash: Text) -> Tuple[float, float]:
     ------
     (lat : float, lon : float)
         Geohashed location.
+
+    Example
+    -------
+    >>> from pymove.utils.geoutils import _decode
+    >>> geoHash_1 = '7pkddb6356fyzxq'
+    >>> geoHash_2 = '7pkd7t2mbj0z1v7'
+    >>> print(_decode(geoHash_1), type(_decode(geoHash_1)))
+    ('-3.777736', '-38.547792') <class 'tuple'>
+    >>> print(_decode(geoHash_2), type(_decode(geoHash_2)))
+    ('-3.793388', '-38.517722') <class 'tuple'>
     """
     return gh.decode(geohash)
 
@@ -189,6 +233,20 @@ def _bin_geohash(lat: float, lon: float, precision: Optional[float] = 15) -> nda
     ------
     array
         Returns a binary geohash array
+
+    Example
+    -------
+    >>> from pymove.utils.geoutils import _bin_geohash
+    >>> lat1, lon1 = -3.777736, -38.547792
+    >>> lat2, lon2 = -3.793388, -38.517722
+    >>> print(_bin_geohash(lat1,lon1), type(_bin_geohash(lat1,lon1)))
+    [0 0 1 1 1 1 1 0 0 1 1 0 1 0 0 0 1 1 0 1 0 1 1 0 1 0 1 0 1 1 0 0 1 1 0 0 0
+    0 1 1 0 0 1 0 1 0 0 1 1 0 0 1 1 1 1 1 0 0 0 1 0 1 0 0 0 1 1 1 0 0 0 0 1 1
+    1 0 1 0] <class 'numpy.ndarray'>
+    >>> print(_bin_geohash(lat2,lon2), type(_bin_geohash(lat2,lon2)))
+    [0 0 1 1 1 1 1 0 0 1 1 0 1 0 0 0 1 1 0 1 0 0 1 1 1 1 1 1 0 1 0 0 0 1 0 1 0
+    1 1 0 0 1 0 1 1 1 0 0 1 1 0 0 0 0 0 1 0 0 0 1 1 0 0 0 0 1 1 1 1 1 1 0 0 1
+    1 1] <class 'numpy.ndarray'>
     """
     hashed = _encode(lat, lon, precision)
     return np.concatenate([BASE_32_TO_BIN[x] for x in hashed])
@@ -211,6 +269,17 @@ def _reset_and_create_arrays_none(
     ------
     arrays
         Returns arrays of none values, of the size of the df.
+
+    Example
+    -------
+    >>> from pymove.utils.geoutils import _reset_and_create_arrays_none
+    >>> print(type(_reset_and_create_arrays_none(geoLife_df)))
+    >>> _reset_and_create_arrays_none(geoLife_df)
+    <class 'tuple'>
+    (array([nan, nan, nan, nan, nan]),
+     array([nan, nan, nan, nan, nan]),
+     array([None, None, None, None, None], dtype=object),
+     array([None, None, None, None, None], dtype=object))
     """
     if reset_index:
         data.reset_index(drop=True, inplace=True)
@@ -244,6 +313,31 @@ def create_geohash_df(data: DataFrame, precision: Optional[float] = 15):
         The input trajectories data
     precision : float, optional
         Number of characters in resulting geohash, by default 15
+
+    Return
+    ------
+    A DataFrame with the additional column 'geohash'
+
+    Example
+    -------
+    >>> from pymove.utils.geoutils import create_geohash_df, _reset_and_create_arrays_none
+    >>> geoLife_df
+                  lat          lon
+        0   39.984094   116.319236
+        1   39.984198   116.319322
+        2   39.984224   116.319402
+        3   39.984211   116.319389
+        4   39.984217   116.319422
+    >>> print(type (create_geohash_df(geoLife_df)))
+    >>> geoLife_df
+    <class 'NoneType'>
+
+                  lat          lon           geohash
+        0   39.984094   116.319236   wx4eqyvh4xkg0xs
+        1   39.984198   116.319322   wx4eqyvhudszsev
+        2   39.984224   116.319402   wx4eqyvhyx8d9wc
+        3   39.984211   116.319389   wx4eqyvhyjnv5m7
+        4   39.984217   116.319422   wx4eqyvhyyr2yy8
     """
     _, _, geohash, _ = _reset_and_create_arrays_none(data)
 
@@ -265,6 +359,30 @@ def create_bin_geohash_df(data: DataFrame, precision: Optional[float] = 15):
         The input trajectories data
     precision : float, optional
         Number of characters in resulting geohash, by default 15
+
+    Return
+    ------
+    A DataFrame with the additional column 'bin_geohash'
+
+    Example
+    -------
+    >>> from pymove.utils.geoutils import create_bin_geohash_df
+    >>> geoLife_df
+                  lat          lon
+        0   39.984094   116.319236
+        1   39.984198   116.319322
+        2   39.984224   116.319402
+        3   39.984211   116.319389
+        4   39.984217   116.319422
+    >>> print(type(create_bin_geohash_df(geoLife_df)))
+    >>> geoLife_df
+    <class 'NoneType'>
+                  lat         lon                                    bin_geohash
+        0   39.984094   116.319236	[1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, ...
+        1   39.984198   116.319322	[1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, ...
+        2   39.984224   116.319402	[1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, ...
+        3   39.984211   116.319389	[1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, ...
+        4   39.984217   116.319422	[1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, ...
     """
     _, _, _, bin_geohash = _reset_and_create_arrays_none(data)
 
@@ -292,6 +410,30 @@ def decode_geohash_to_latlon(
         The name of the feature with hashed trajectories, by default GEOHASH
     reset_index : boolean, optional
         Condition to reset the df index, by default True
+
+    Return
+    ------
+    A DataFrame with the additional columns 'lat_decode' and 'lon_decode'
+
+    Example
+    -------
+    >>> from pymove.utils.geoutils import decode_geohash_to_latlon
+    >>> geoLife_df
+                  lat          lon           geohash
+        0   39.984094   116.319236   wx4eqyvh4xkg0xs
+        1   39.984198   116.319322   wx4eqyvhudszsev
+        2   39.984224   116.319402   wx4eqyvhyx8d9wc
+        3   39.984211   116.319389   wx4eqyvhyjnv5m7
+        4   39.984217   116.319422   wx4eqyvhyyr2yy8
+    >>> print(type(decode_geohash_to_latlon(geoLife_df)))
+    >>> geoLife_df
+    <class 'NoneType'>
+                  lat          lon           geohash  lat_decode   lon_decode
+        0   39.984094   116.319236   wx4eqyvh4xkg0xs   39.984094   116.319236
+        1   39.984198   116.319322   wx4eqyvhudszsev   39.984198   116.319322
+        2   39.984224   116.319402   wx4eqyvhyx8d9wc   39.984224   116.319402
+        3   39.984211   116.319389   wx4eqyvhyjnv5m7   39.984211   116.319389
+        4   39.984217   116.319422   wx4eqyvhyyr2yy8   39.984217   116.319422
     """
     if label_geohash not in data:
         raise ValueError('feature {} not in df'.format(label_geohash))
