9b734cbe3648c88c96599bdfddedfe816b08902a
pedroccufc <pedro_alves96@yahoo.com.br>
2021-05-20 14:26:59 -0300

    unit tests

--- pymove/tests/test_utils_networkx.py
+++ pymove/tests/test_utils_networkx.py
@@ -1,3 +1,52 @@
+import numpy as np
+import pandas as pd
+
+from unittest import TestCase
+
+from pandas.testing import assert_frame_equal
+
+from networkx.classes.digraph import DiGraph
+from networkx.testing import assert_graphs_equal
+
+from pymove.utils.networkx import _populate_graph
+from pymove.utils.networkx import build_transition_graph_from_dict
+from pymove.utils.networkx import build_transition_graph_from_df
+from pymove.utils.networkx import graph_to_dict
+from pymove.utils.networkx import get_all_paths
+
+from pymove.utils.constants import (
+    DATETIME, 
+    LATITUDE, 
+    LONGITUDE, 
+    LOCAL_LABEL, 
+    PREV_LOCAL,
+    TID_STAT,
+    TRAJ_ID
+)
+
+dict_graph = {
+    'nodes': {
+        'datetime': {
+            '2': ['2020-01-01 09:10:15'], '4': ['2020-01-01 09:15:45'],
+            '6': ['2020-01-01 09:25:30'], '8': ['2020-01-01 09:30:17'],
+            '9': ['2020-01-01 09:45:16']},
+        'coords': {
+            '2': (3.1234567165374756, 38.12345504760742),
+            '4': (3.1234567165374756, 38.12345504760742),
+            '6': (3.1234567165374756, 38.12345504760742),
+            '8': (3.1234567165374756, 38.12345504760742),
+            '9': (3.1234567165374756, 38.12345504760742)},
+        'freq_source': {
+            '2': 1, '4': 0, '6': 0, '8': 0, '9': 0},
+        'freq_target': {
+            '2': 0, '4': 0, '6': 0, '8': 0, '9': 1}}, 
+    'edges': {
+        '2': {'4': {'weight': 1, 'mean_times': '0 days 00:05:30'}},
+        '4': {'6': {'weight': 1, 'mean_times': '0 days 00:09:45'}},
+        '6': {'8': {'weight': 1, 'mean_times': '0 days 00:04:47'}},
+        '8': {'9': {'weight': 1, 'mean_times': '0 days 00:14:59'}}}
+}
+
 list_data1 = {
     TRAJ_ID: [['d95bafc8f2a4d27bdcf4bb99f4bea973', 'd95bafc8f2a4d27bdcf4bb99f4bea973',
                'd95bafc8f2a4d27bdcf4bb99f4bea973', 'd95bafc8f2a4d27bdcf4bb99f4bea973',
@@ -70,7 +119,7 @@ def test__populate_graph():
 
 
 def test_build_transition_graph_from_dict():
-    expected_graph = transition_graph()
+    expected_graph = _transition_graph()
 
     graph = build_transition_graph_from_dict(dict_graph)
 
@@ -78,7 +127,7 @@ def test_build_transition_graph_from_dict():
 
 
 def test_build_transition_graph_from_df():
-    expected_graph = transition_graph()
+    expected_graph = _transition_graph()
 
     data = pd.DataFrame(list_data1)
 
@@ -88,7 +137,7 @@ def test_build_transition_graph_from_df():
 
 
 def test_graph_to_dict():
-    graph = transition_graph()
+    graph = _transition_graph()
 
     expected_dict = {
         'nodes': {
